49eecc60f6a191eb2f1c071d05cfa3e2e3a0b5bd
Lombardi, Lucas <lucas.lombardi@lmi.org>
2024-09-13 23:27:40 -0400

    finalized 1st page no styling

--- Page 1/scripts.js
+++ Page 1/scripts.js
@@ -1,39 +1,44 @@
 const ingredients = []; // store ingredients in here...
+let cookingTime = 0; // finalized numerical value to be fed to chatGPT
 
 window.onload = function(){
     addIngredientBox(false); // automatically add the first textbox on load
 }
 
-function addIngredientBox() {
+function addIngredientBox(allowRemove = True) {
     const container = document.getElementById("ingredientBoxContainer");
 
     // hold textbox and buttons!
     const ingredientBoxDiv = document.createElement("div");
     ingredientBoxDiv.className = "ingredient-box"; // styling class
 
-    // + button
-    const addButton = document.createElement("button");
-    addButton.innerHTML = "+"; 
-    addButton.className = "add-button";
-    addButton.onclick = addIngredientBox; // when you click the + button it will add another box
-
     // textbox input placeholder
     const ingredientInput = document.createElement("input");
     ingredientInput.type = "text";
     ingredientInput.className = "ingredient-input"; // styling class
 
-    // remove button with the "trashcan"
-    const removeButton = document.createElement("button");
-    removeButton.innerHTML = "🗑️";
-    removeButton.className = "remove-button";
-    removeButton.onclick = function() {
-        container.removeChild(ingredientBoxDiv); // remove the entire div
-    };
+    // we want to create a "+" button only for the first input
+    const addButton = document.createElement("button");
+    addButton.innerHTML = "+"; 
+    addButton.className = "add-button";
+    addButton.onclick = () => addIngredientBox(true); // when you click the + button it will add another box
 
+    // let's add the + button now to the first input
     ingredientBoxDiv.appendChild(addButton);
     ingredientBoxDiv.appendChild(ingredientInput);
-    ingredientBoxDiv.appendChild(removeButton);
 
+    // if we can, add a remove button (THIS WONT WORK FOR THE FIRST BOX)
+    if (allowRemove) {
+        const removeButton = document.createElement("button");
+        removeButton.innerHTML = "🗑️";
+        removeButton.className = "remove-button";
+        removeButton.onclick = function() {
+            container.removeChild(ingredientBoxDiv); // remove the entire div
+        };
+        ingredientBoxDiv.appendChild(removeButton);
+    }
+
+    // append to div to the container
     container.appendChild(ingredientBoxDiv);
 }
 
@@ -47,9 +52,22 @@ function cook() {
         }
     });
 
+    // get time value
+    const timeToCook = document.getElementById("timeToCook").value;
+
+    // ensure the time is a real number
+    if (!timeToCook || isNaN(timeToCook) || Number(timeToCook) <= 0) {
+        alert("Please enter a valid cooking time!");
+        return;
+    } 
+    
     const message = ingredients.length > 0
         ? `You can make something with: ${ingredients.join(", ")}`
         : "You need to add ingredients first.";
 
+    cookingTime = Number(timeToCook);
+
+    alert(ingredients);
+    alert(cookingTime);
     alert(message);
 }
